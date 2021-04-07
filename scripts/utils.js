c85a000b55d7481d7f3c31d4d19c3b1f03006006
s1r1us <22428507+msrkp@users.noreply.github.com>
2021-04-07 12:38:33 +0530

    update db

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,7 +2,7 @@ var patternMatch = (resp, db) => {
     var result = ""
     for(i=0;i<db.length;i++){
         var res = [];
-        var re = new RegExp(db[i].pattern)
+        var re = new RegExp(atob(db[i].base64))
         match = re.exec(resp);
         if(match){ 
             result += db[i].library + ", "
