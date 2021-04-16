ce4ddac929b61a5c24e87b51b174a7e3ea601808
posix-lee <posix.research@gmail.com>
2021-04-16 22:34:32 +0900

    script optimized

--- scripts/background.js
+++ scripts/background.js
@@ -1,148 +1,64 @@
-const dbUrl = "https://raw.githubusercontent.com/msrkp/PPScan/main/db.json";
-var passiveFound = {}
-var db;
-var found = new Set();
+const databaseUrl = chrome.extension.getURL('/database.csv');
 
-var download = (url) => {
-    return new Promise((resolve, reject) => {
-        fetch(url).then(
-                response => response.text()
-            ).then(data => {
-                resolve(data);
-            })
-            .catch(err => {
-                reject(new Error('Error downloading ' + url))
-            })
-    });
-}
+/*  initialize */
+const found = new Set();
 
-var downloadDb = (url) => {
-    return new Promise((resolve, reject) => {
-        fetch(url).then(
-                response => response.json()
-            ).then(data => {
-                db = data;
-                resolve('success');
-            })
-            .catch(err => {
-                reject(new Error('Error downloading ' + url))
-            })
-    });
-}
+setBadgeCount(0);
 
-var check = (req) => {
-    var url = new URL(req.url);
-    if (url.protocol == "http:" || url.protocol == "https:") {
-        download(url).then(resp => {
-            var tmp = patternMatch(resp, db);
-            if (tmp.length > 0) {
-                console.log(req, req["initiator"]);
-                found.add("Script from " + new URL(req["url"]).origin + " using " + tmp + " at " + req["initiator"] + " is vulnerable!")
-                setNum(found.size);
-                return;
-            }
-        })
-    }
-
-}
-
-
-
-function setNum(len) {
-    chrome.browserAction.setBadgeText({ "text": '' + len });
-    if (len > 0) {
-        chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-    } else {
-        chrome.browserAction.setBadgeBackgroundColor({ color: [0, 0, 255, 0] });
-    }
-}
-
-function isCSPHeader(headerName) {
-    return (headerName === 'CONTENT-SECURITY-POLICY');
-}
-
-function isXFrame(headerName) {
-    return (headerName === 'X-FRAME-OPTIONS');
-}
-
-function isCaching(headerName) {
-    return (headerName === 'IF-NONE-MATCH');
-}
-
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    console.log(msg);
+/* setup listeners */
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
     found.add(msg);
-    setNum(found.size);
+    setBadgeCount(found.size);
 });
 
-chrome.extension.onConnect.addListener(function(port) {
-    console.log('connected ', port);
+chrome.extension.onConnect.addListener((port) => {
+    console.log('[>] New Session ', port);
     if (port.name == "logger") {
-        port.onMessage.addListener(function(msg) {
-            console.log(msg)
-            console.log(found);
+        port.onMessage.addListener((msg) => {
+            if (msg == 'clearLog') {
+                found.clear();
+                setBadgeCount(0);
+            }
             port.postMessage({ found: [...found] });
         });
     }
 });
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({ toggle: false }, function() {
-        console.log('toggle off');
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ toggle: false }, () => {
+        console.log('[*] Toggle: disabled');
     });
-    chrome.storage.sync.set({ buster: false }, function() {
-        console.log('window mode off');
+    chrome.storage.sync.set({ buster: false }, () => {
+        console.log('[*] Buster: disabled');
     });
-    chrome.storage.sync.set({ passive: true }, function() {
-        console.log('passive mode on');
+    chrome.storage.sync.set({ passive: true }, () => {
+        console.log('[*] Passive: enabled');
     });
 });
-chrome.storage.sync.get("toggle", function(data) {
-    if (data.toggle) {
 
-
-        // Listens on new request
-        chrome.webRequest.onHeadersReceived.addListener((details) => {
-            for (let i = 0; i < details.responseHeaders.length; i += 1) {
-                if (isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
-                    const csp = 'default-src * \'unsafe-inline\' \'unsafe-eval\' data: blob:; ';
-                    details.responseHeaders[i].value = csp;
-                }
-                if (isXFrame(details.responseHeaders[i].name.toUpperCase())) {
-                    details.responseHeaders[i].name = 'foo';
-
-                }
-                if (isCaching(details.responseHeaders[i].name.toUpperCase())) {
-                    details.responseHeaders[i].name = 'foo';
-
-                }
-            }
-            return { // Return the new HTTP header
-                responseHeaders: details.responseHeaders,
+chrome.storage.sync.get("toggle", ({ toggle }) => {
+    if (toggle) {
+        chrome.webRequest.onHeadersReceived.addListener((response) => {
+            response.forEach(header => {
+                if (isCSPHeader(header)) {
+                    header.value = `default-src * 'unsafe-inline' 'unsafe-eval' data: blob:; `;
+                };
+                if (isXFrameEnabled(header)) {
+                    header.name = 'foo';
+                };
+                if (isCached(header)) {
+                    header.name = 'foo';
+                };
+            });
+            return {
+                responseHeaders: header.responseHeaders,
             };
         }, {
             urls: ['<all_urls>']
         }, ['blocking', 'responseHeaders', 'extraHeaders']);
-
     }
-
 });
 
-chrome.storage.sync.get("passive", function(data) {
-    if (data.passive) {
-        downloadDb(dbUrl).then(() => {
-                var filter = {
-                    urls: ["<all_urls>"],
-                    types: ["script"]
-                };
-                var scan = (req) => {
-                    if (req.method == "GET") {
-                        check(req);
-                    }
-                }
-
-                chrome.webRequest.onCompleted.addListener(scan, filter, []);
-            })
-            .catch(e => console.log(e));
-    }
+chrome.storage.sync.get("passive", ({ passive }) => {
+    if (passive) { updateDB(); }
 });
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,13 +1,132 @@
-var patternMatch = (resp, db) => {
-    var result = ""
-    for(i=0;i<db.length;i++){
-        var res = [];
-        var re = new RegExp(atob(db[i].base64))
-        match = re.exec(resp);
-        if(match){ 
-            result += db[i].library + ", "
-        }
-    }    
-    return result;
+const blacklist = [
+    'https://www.googleadservices.com/pagead/conversion_async.js'
+];
 
-}
+let database = [];
+
+const patternMatch = (response, database) => {
+    const result = [];
+    const matches = [];
+
+    database.forEach((pattern) => {
+        const { name, type, chunk } = pattern;
+
+        switch (type) {
+            case 'regex':
+                const re = new RegExp(atob(chunk));
+                const match = re.exec(response);
+
+                if (match) {
+                    result.push(name);
+                    matches.push(match);
+                };
+                break;
+            case 'text':
+                const position = response.indexOf(chunk);
+
+                if (position != -1) {
+                    result.push(name);
+                    matches.push({ index: position });
+                };
+                break;
+        }
+    });
+
+    return [result, matches];
+};
+
+const downloadDb = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url).then(
+                response => response.text()
+            ).then(res => {
+                const ret = res.split('\n').map(line => {
+                    const [name, type, chunk] = line.split('|', 3);
+                    return { name, type, chunk };
+                });
+                resolve(ret);
+            })
+            .catch(err => {
+                console.log(err);
+                reject(new Error('Error downloading ' + url))
+            })
+    });
+};
+
+const download = (url) => {
+    return new Promise((resolve, reject) => {
+        fetch(url).then(
+                response => response.text()
+            ).then(data => {
+                resolve(data);
+            })
+            .catch(err => {
+                reject(new Error('Error downloading ' + url))
+            })
+    });
+};
+
+const check = ({ requestUri, initiator }) => {
+    const url = new URL(requestUri);
+
+    if (blacklist.indexOf(url.origin + url.pathname) != -1) {
+        return;
+    };
+    if (!url.hostname || !url.pathname) {
+        return;
+    };
+    if (url.protocol == "http:" || url.protocol == "https:") {
+        download(url).then(res => {
+            const [result, match] = patternMatch(res, database);
+
+            result.forEach((name, i) => {
+                found.add(`[${name}] ${initiator}\n${requestUri}:${match[i].index}`)
+                setBadgeCount(found.size);
+            });
+        })
+    }
+};
+
+const filter = {
+    urls: ["<all_urls>"],
+    types: ["script"]
+};
+
+const scan = ({ method, url, initiator }) => {
+    if (method == "GET") {
+        check({ requestUri: url, initiator });
+    }
+};
+
+const updateDB = () => {
+    if (!database.length) {
+        downloadDb(databaseUrl).then((_database) => {
+            database = _database;
+            chrome.webRequest.onCompleted.addListener(scan, filter, []);
+        }).catch(e => console.log(e));
+    }
+};
+
+const setBadgeCount = (len) => {
+    chrome.browserAction.setBadgeText({ "text": '' + len });
+    chrome.browserAction.setBadgeBackgroundColor({ color: len > 0 ? [255, 0, 0, 255] : [0, 0, 255, 0] });
+
+    return;
+};
+
+const maybeSame = (a, b) => {
+    return a.touppercase().trim() == b.touppercase().trim();
+};
+
+const isCSPHeader = ({ header: name }) => {
+    console.log(name);
+    return maybeSame(name, 'Content-Security-Policy');
+};
+
+const isXFrameEnabled = ({ header: name }) => {
+    return maybeSame(name, 'X-Frame-Options');
+};
+
+const isCached = ({ header: name }) => {
+    return maybeSame(name, 'If-None-Match');
+};
